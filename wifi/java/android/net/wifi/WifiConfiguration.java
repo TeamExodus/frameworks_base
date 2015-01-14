05c44a07240c5660383dc4259cf9dd576838bf4c
Bruno Randolf <br1@einfach.org>
2015-01-13 16:54:21 -0800

    wifi: Framework support for Ad-Hoc WiFi (IBSS mode)
     * Backport from previous CM releases, adapted to new API
    
    Change-Id: Id175bbfb7500329d5f8abfe645106854c201b2fb
    Signed-off-by: Steve Kondik <steve@cyngn.com>

--- wifi/java/android/net/wifi/WifiConfiguration.java
+++ wifi/java/android/net/wifi/WifiConfiguration.java
@@ -58,6 +58,10 @@ public class WifiConfiguration implements Parcelable {
     public static final String pmfVarName = "ieee80211w";
     /** {@hide} */
     public static final String updateIdentiferVarName = "update_identifier";
+     /** {@hide} */
+    public static final String modeVarName = "mode";
+    /** {@hide} */
+    public static final String frequencyVarName = "frequency";
     /** {@hide} */
     public static final int INVALID_NETWORK_ID = -1;
 
@@ -301,6 +305,18 @@ public class WifiConfiguration implements Parcelable {
      * @hide
      */
     public String updateIdentifier;
+
+   /**
+     * This is an Ad-Hoc (IBSS) network
+     * {@hide}
+     */
+    public boolean isIBSS;
+
+    /**
+     * Frequency of the Ad-Hoc (IBSS) network, if newly created
+     * {@hide}
+     */
+    public int frequency;
 
     /**
      * The set of key management protocols supported by this configuration.
@@ -910,6 +926,8 @@ public class WifiConfiguration implements Parcelable {
         roamingConsortiumIds = new long[0];
         priority = 0;
         hiddenSSID = false;
+        isIBSS = false;
+        frequency = 0;
         disableReason = DISABLED_UNKNOWN_REASON;
         allowedKeyManagement = new BitSet();
         allowedProtocols = new BitSet();
@@ -1453,6 +1471,8 @@ public class WifiConfiguration implements Parcelable {
             wepTxKeyIndex = source.wepTxKeyIndex;
             priority = source.priority;
             hiddenSSID = source.hiddenSSID;
+            isIBSS = source.isIBSS;
+            frequency = source.frequency;
             allowedKeyManagement   = (BitSet) source.allowedKeyManagement.clone();
             allowedProtocols       = (BitSet) source.allowedProtocols.clone();
             allowedAuthAlgorithms  = (BitSet) source.allowedAuthAlgorithms.clone();
@@ -1554,6 +1574,8 @@ public class WifiConfiguration implements Parcelable {
         dest.writeInt(wepTxKeyIndex);
         dest.writeInt(priority);
         dest.writeInt(hiddenSSID ? 1 : 0);
+        dest.writeInt(isIBSS ? 1 : 0);
+        dest.writeInt(frequency);
         dest.writeInt(requirePMF ? 1 : 0);
         dest.writeString(updateIdentifier);
 
@@ -1630,6 +1652,8 @@ public class WifiConfiguration implements Parcelable {
                 config.wepTxKeyIndex = in.readInt();
                 config.priority = in.readInt();
                 config.hiddenSSID = in.readInt() != 0;
+                config.isIBSS = in.readInt() != 0;
+                config.frequency = in.readInt();
                 config.requirePMF = in.readInt() != 0;
                 config.updateIdentifier = in.readString();
 
