27f70e899f1c271f2a0c8be1e466209c6a866954
Dave Kessler <activethrasher00@gmail.com>
2016-03-27 01:02:30 -0400

    fix some telephony errors
    Change-Id: Ie58f6f40fb55113fc5e21b38f75ba7d61bc7161a

--- Android.mk
+++ Android.mk
@@ -405,6 +405,7 @@ LOCAL_SRC_FILES += \
 	telephony/java/com/android/internal/telephony/ISub.aidl \
 	telephony/java/com/android/internal/telephony/ITelephony.aidl \
 	telephony/java/com/android/internal/telephony/ITelephonyRegistry.aidl \
+	telephony/java/com/android/internal/telephony/IExtTelephony.aidl \
 	telephony/java/com/android/internal/telephony/IWapPushManager.aidl \
 	wifi/java/android/net/wifi/IWifiManager.aidl \
 	wifi/java/android/net/wifi/passpoint/IWifiPasspointManager.aidl \
