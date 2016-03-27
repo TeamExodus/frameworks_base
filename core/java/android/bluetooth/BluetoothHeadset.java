30911a65280a3308ede1ac6181e785f852e2c81c
Dave Kessler <activethrasher00@gmail.com>
2016-03-27 12:09:46 -0400

    fix some missing constants in bluetooth
    Change-Id: Ie16e150ec3f35fa584e6479fc184c29953adc23c

--- core/java/android/bluetooth/BluetoothHeadset.java
+++ core/java/android/bluetooth/BluetoothHeadset.java
@@ -46,7 +46,7 @@ import java.util.List;
 public final class BluetoothHeadset implements BluetoothProfile {
     private static final String TAG = "BluetoothHeadset";
     private static final boolean DBG = true;
-    private static final boolean VDBG = false;
+    private static final boolean VDBG = true;
 
     /**
      * Intent used to broadcast the change in connection state of the Headset
@@ -128,6 +128,13 @@ public final class BluetoothHeadset implements BluetoothProfile {
             "android.bluetooth.headset.action.VENDOR_SPECIFIC_HEADSET_EVENT";
 
     /**
+     * @hide Broadcast intent when HF indicator value changed is updated by HS.
+     */
+    @SdkConstant(SdkConstantType.BROADCAST_INTENT_ACTION)
+    public static final String ACTION_HF_INDICATOR_VALUE_CHANGED =
+            "codeaurora.bluetooth.headset.action.ACTION_HF_INDICATOR_VALUE_CHANGED";
+
+    /**
      * A String extra field in {@link #ACTION_VENDOR_SPECIFIC_HEADSET_EVENT}
      * intents that contains the name of the vendor-specific command.
      */
@@ -196,6 +203,20 @@ public final class BluetoothHeadset implements BluetoothProfile {
      * A vendor-specific command for unsolicited result code.
      */
     public static final String VENDOR_RESULT_CODE_COMMAND_ANDROID = "+ANDROID";
+
+    /**
+     * @hide Used for sharing the HF indicator assigned number.
+     */
+    public static final String HF_INDICATOR_ASSIGNED_NUMBER  =
+            "codeaurora.bluetooth.headset.intent.category.anum";
+
+
+    /**
+     * @hide Used for sharing the HF indicator assigned number's value.
+     */
+    public static final String HF_INDICATOR_ASSIGNED_NUMBER_VALUE  =
+            "codeaurora.bluetooth.headset.intent.category.anumvalue";
+
 
     /**
      * Headset state when SCO audio is not connected.
